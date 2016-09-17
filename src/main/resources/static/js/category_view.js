f60273406c2f1d4a309939fa76796b07c5576d45
Nicolas Haiduchok <nico13051995@mail.com>
2016-09-17 14:29:39 +0300

    //

--- src/main/resources/static/js/category_view.js
+++ src/main/resources/static/js/category_view.js
@@ -1,21 +1,24 @@
-function checkSubmitAvailable(){
-       var submitButton = $('#addTopicButton');
-if ( CKEDITOR.instances.ckeditor.getData() ==='' ||  $('#topic_name').val()==='')
-    submitButton.hide();
-else submitButton.show();
+function checkSubmitAvailable() {
+    var submitButton = $('#addTopicButton');
+    if (CKEDITOR.instances.ckeditor.getData() === '' || $('#topic_name').val() === '')
+        submitButton.hide();
+    else submitButton.show();
 }
-function topicAdditionSuccess(topicId){
-  Materialize.toast('Тему успішно додано', 4000,'green-toast') // 4000 is the duration of the toast
-  GoToUrl(serverPrefix+'/view/topic/'+topicId);
+
+function topicAdditionSuccess(topicId) {
+    Materialize.toast('Тему успішно додано', 4000, 'green-toast') // 4000 is the duration of the toast
+    GoToUrl(serverPrefix + '/view/topic/' + topicId);
 }
-function topicAdditionFail(){
- Materialize.toast('Не вдалось додати тему', 4000,'red-toast') // 4000 is the duration of the toast
+
+function topicAdditionFail() {
+    Materialize.toast('Не вдалось додати тему', 4000, 'red-toast') // 4000 is the duration of the toast
 }
-function addTopic(event,url){
+
+function addTopic(event, url) {
     event.preventDefault();
     for (instance in CKEDITOR.instances)
         CKEDITOR.instances[instance].updateElement();
-submitForm('#addTopicForm',url,topicAdditionSuccess,topicAdditionFail);
+    submitForm('#addTopicForm', url, topicAdditionSuccess, topicAdditionFail);
 }
 
 
@@ -23,13 +26,21 @@ $(document).ready(function() {
     var cke = initCkEditor("ckeditor", 200);
     checkSubmitAvailable();
 
-        CKEDITOR.instances.ckeditor.on('change', function(evt) { 
- checkSubmitAvailable();
-});
-        $('#topic_name').bind('textchange', function (event, previousText) {
-    checkSubmitAvailable();
-});
-   
+    CKEDITOR.instances.ckeditor.on('change', function(evt) {
+        checkSubmitAvailable();
+    });
+    $('#topic_name').bind('textchange', function(event, previousText) {
+        checkSubmitAvailable();
+    });
+    $('.topic-divided')
+    .click(function(e) {
+        var target = $(this).attr("target")
+        $('#' + target).toggleClass('hide-me').toggleClass('half');
+        
+        e.stopPropagation();
+    });
+
+
     /*var submitButton = $('#addTopicButton');
     if (typeof sumbitButton.val() != 'undefined' && sumbitButton.val().length == 0)
         sumbitButton.hide();*/
